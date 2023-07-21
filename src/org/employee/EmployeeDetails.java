45ffbe9d8a675831a1460f87c52a921b4712bb40
anandgitacc <anand.akmail@gmail.com>
2023-07-21 20:42:29 +0530

    firstgit

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package org.employee;
+
+public class EmployeeDetails {
+	
+	// private -> access specifier
+	// void    -> return type of given method
+	// empName -> method name
+	// ()      -> arguments place / input or parameter place
+	// {}      -> block or body of the method
+	private void empName() {
+		// implementation detail / business logic
+		// "" - double quote - String (predefined class)
+		System.out.println("Employee name: Anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("Employee mobile: 8939278836");
+	}
+	
+	public static void main(String[]  args) {
+		
+		// object
+		EmployeeDetails e = new EmployeeDetails();
+		// method call
+		e.empName();
+		e.empMobile();
+		
+	}
+	
+
+}
