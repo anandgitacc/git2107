272b71f5e4e4b6956f811879636f0f415920b864
anandgitacc <anand.akmail@gmail.com>
2023-07-22 09:41:02 +0530

    sivagit

--- src/org/employee/EmployeeDetails.java
+++ src/org/employee/EmployeeDetails.java
@@ -17,6 +17,10 @@ public class EmployeeDetails {
 		System.out.println("Employee mobile: 8939278836");
 	}
 	
+	private void empLocation() {
+		System.out.println("location: OMR");
+	}
+	
 	public static void main(String[]  args) {
 		
 		// object
